6c562e1a7fcc1e361522de8575133031c7377f8f
github <calbest31@outlook.com>
2021-04-22 23:59:24 -0400

    grabbed DOM elements and connected key

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const searchList = $('#cityList');
+const searchInput = $('#citySearch');
+const searchBtn = $('#citySearchBtn');
+const clearSearch = $('#clearHist');
+
+const mainWeathEl = $('#mainWeath')
+const cityName = $('#mainName');
+const cityTemp = $('#mainTemp');
+const cityHumid = $('#mainHumid');
+const cityUV = $('#mainUV');
+
+const key = '4529e17e22e457ac065d45b98973fed0';
